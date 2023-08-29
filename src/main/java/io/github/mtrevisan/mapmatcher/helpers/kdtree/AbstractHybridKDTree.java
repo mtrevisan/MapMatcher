7bb6cb9e25889f9271710e6e52beb9a2fc873e06
Mauro Trevisan <mauro.simioni@targatelematics.com>
2023-08-29 17:32:51 +0200

    refactor
    refactor
    
    refactor

--- src/main/java/io/github/mtrevisan/mapmatcher/helpers/kdtree/AbstractHybridKDTree.java
+++ src/main/java/io/github/mtrevisan/mapmatcher/helpers/kdtree/AbstractHybridKDTree.java
@@ -5,9 +5,9 @@ import io.github.mtrevisan.mapmatcher.helpers.quadtree.Region;
 import io.github.mtrevisan.mapmatcher.spatial.Point;
 
 import java.util.Collection;
+import java.util.List;
 
 
-//TODO
 public class AbstractHybridKDTree{
 
 	public void insert(final RegionTree tree, final Region region){
@@ -15,55 +15,40 @@ public class AbstractHybridKDTree{
 	}
 
 	public void insert(final RegionTree tree, final Region region, final Point point){
-		final Collection<Region> regions = regionsInRange(tree, region);
+		final Collection<Region> regions = query(tree, region);
 
 		if(!regions.isEmpty())
 			for(final Region queriedRegion : regions)
 				if(queriedRegion.isBoundary()){
-					final KDTree terminalTree = KDTree.ofEmpty(2);
+					final KDTree terminalTree = KDTree.ofEmpty(point.getDimensions());
 					final KDNode parentNode = (KDNode)queriedRegion.getNode();
 					terminalTree.insert(parentNode, point);
 					return;
 				}
 
+		//TODO extends Region, parké kusita 'l fà senso (?)
+		//	ge xé un poblèma su RegionQuadTree.split(), kuando ke se krea un Region...
 		region.setBoundary();
 		region.setNode(new KDNode(point));
 		tree.insert(region);
 	}
 
-//	public void insert(final RegionTree tree, final Region region, final List<Point> points){
-//		final Collection<Region> regions = regionsInRange(tree, region);
-//
-//		if(!regions.isEmpty())
-//			for(final Region queriedRegion : regions)
-//				if(queriedRegion.isBoundary()){
-//					final KDNode parentNode = (KDNode)queriedRegion.getNode();
-//					final KDTree terminalTree = KDTree.of(parentNode, points);
-//					return;
-//				}
-//
-//		region.setBoundary();
-//		region.setNode(new KDNode(points));
-//		tree.insert(region);
-//	}
 
-
-	public Collection<Region> regionsInRange(final RegionTree tree, final Region region){
+	public Collection<Region> query(final RegionTree tree, final Region region){
 		return tree.query(region);
 	}
 
 	public boolean contains(final RegionTree tree, final Region region, final Point point){
-		final Collection<Region> regions = regionsInRange(tree, region);
+		final Collection<Region> regions = query(tree, region);
 
 		if(!regions.isEmpty())
 			for(final Region queriedRegion : regions)
 				if(queriedRegion.isBoundary()){
-					final KDTree kdTree = KDTree.ofEmpty(2);
+					final KDTree terminalTree = KDTree.ofEmpty(point.getDimensions());
 					final KDNode kdNode = (KDNode)queriedRegion.getNode();
-					if(kdTree.contains(kdNode, point))
+					if(terminalTree.contains(kdNode, point))
 						return true;
 				}
-
 		return false;
 	}
 
